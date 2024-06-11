2802999d1951716125d9891575ec69137a19064d
Joshua Zega <hedywidja321@gmail.com>
2024-06-11 22:46:56 +0700

    Commit untuk cadangan sebelum penggunaan bootstrap untuk header dan footer

--- #inc/controllers/Beranda.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Beranda extends Controller{
-    public function index(){
-        $data['title'] = 'PPID | PERUM LKBN ANTARA';
-        $this->view('header', $data);
-        $this->view('beranda/index');
-        $this->view('footer');
-    }
-}
--- #inc/page.php
+++ #inc/page.php
@@ -1,45 +1,48 @@
 <?php
 
-class Tentang extends Controller{
+class Page extends Controller{
+
+
     public function index(){
         $data['title'] = 'PPID | PERUM LKBN ANTARA';
         $this->view('header', $data);
-        $this->view('beranda/index');
+        $this->view('page/index');
         $this->view('footer');
     }
 
     public function profil(){
         $data['title'] = 'PPID | Profil';
         $this->view('header', $data);
-        $this->view('tentang/profil');
+        $this->view('page/profil');
         $this->view('footer');
     }
 
     public function tugas_dan_fungsi(){
         $data['title'] = 'PPID | Tugas dan Fungsi';
         $this->view('header', $data);
-        $this->view('tentang/tugas_dan_fungsi');
+        $this->view('page/tugas_dan_fungsi');
         $this->view('footer');
     }
 
     public function visi_dan_misi(){
         $data['title'] = 'PPID | Visi dan Misi';
         $this->view('header', $data);
-        $this->view('tentang/visi_dan_misi');
+        $this->view('page/visi_dan_misi');
         $this->view('footer');
     }
 
     public function struktur_organisasi(){
         $data['title'] = 'PPID | Struktur Organisasi';
         $this->view('header', $data);
-        $this->view('tentang/struktur_organisasi');
+        $this->view('page/struktur_organisasi');
         $this->view('footer');
     }
 
     public function regulasi(){
         $data['title'] = 'PPID | Regulasi';
         $this->view('header', $data);
-        $this->view('tentang/regulasi');
+        $this->view('page/regulasi');
         $this->view('footer');
     }
+
 }
